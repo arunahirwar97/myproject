e604455adb8c19f989872029f4ba16c5d1e08a3f
websiteaap <arun887126@gmail.com>
2022-05-21 21:02:19 +0530

    Add

--- mysec.py
+++ mysec.py
@@ -1,3 +1,3 @@
 API_URL = "https://api.chat-api.com/instance350463/"
 TOKEN = "weog7fu3yw2zhqxn"
-MONGO_URI="mmongodb+srv://arunnotebote:p15dT5wvxjlCNZKB@cluster0.3h5co.mongodb.net/?retryWrites=true&w=majority"
+MONGO_URI="mmongodb+srv://root:root@cluster0.3h5co.mongodb.net/?retryWrites=true&w=majority"
