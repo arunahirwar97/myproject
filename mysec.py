f3de74ce5d0352f9112723cfc50d94987fcd8f2f
ARUN AHIRWAR <53229705+arunahirwar97@users.noreply.github.com>
2025-05-20 11:25:41 +0530

    Update mysec.py

--- mysec.py
+++ mysec.py
@@ -1,3 +1,3 @@
 API_URL = "https://api.chat-api.com/instance350463/"
 TOKEN = "weog7fu3yw2zhqxn"
-MONGO_URI="mmongodb+srv://root:root@cluster0.3h5co.mongodb.net/?retryWrites=true&w=majority"
+MONGO_URI=""
